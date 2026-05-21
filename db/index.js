d2a67698a472d5261fc1cc903754f082de5aec8f
sregel1982-hub <sregel1982@gmail.com>
2026-05-21 22:34:04 +0200

    Update index.js

--- db/index.js
+++ db/index.js
@@ -1,12 +1,6 @@
-process.env.NETLIFY) {
-  const { connect } = await import("@netlify/database");
-  const { drizzle } = await import("drizzle-orm/netlify-db");
-  const client = connect();
-  db = drizzle(client);
-} else {
-  const pg = await import("pg");
-  const { drizzle } = await import("drizzle-orm/node-postgres");
-  const Pool = pg.default.Pool;
-  const pool = new Pool({ connectionString: process.env.DATABASE_URL });
-  db = drizzle(pool);
-}
+import pg from "pg";
+import { drizzle } from "drizzle-orm/node-postgres";
+
+const { Pool } = pg;
+const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+export const db = drizzle(pool);
