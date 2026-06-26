36bf6ba4c9cbbe2190b2adef41aa6659e2f3b834
sregel1982-hub <sregel1982@gmail.com>
2026-06-26 20:34:15 +0200

    Update utils.js

--- netlify/functions/utils.js
+++ netlify/functions/utils.js
@@ -1,4 +1,4 @@
- import { createClient } from "@supabase/supabase-js";
+import { createClient } from "@supabase/supabase-js";
 
 export function jsonResponse(data, status = 200) {
   return new Response(JSON.stringify(data), {
